390ede4aca2058b7e66e600274dba8da23aedd6f
Mike Ryu <mryu@westmont.edu>
2025-04-09 23:56:30 -0700

    Fixed misleading JavaDocs for BasicQueue.

--- BasicQueue.java
+++ BasicQueue.java
@@ -11,14 +11,23 @@
 public interface BasicQueue extends Peekable {
 
   /**
-   * Inserts the specified element into the head of this queue.
+   * Retrieves and removes an element at the "end" of this queue.
+   * <br><br>
+   * <b>NOTE</b> It is not exactly important where in the underlying ordered collection
+   * data structure this operation occurs at, as long as it is at the logical opposite end
+   * of the location where {@link BasicQueue#dequeue()} would occur at.
    *
    * @param o the element to add
    */
   void enqueue(Object o);
 
   /**
-   * Retrieves and removes the head of this queue.
+   * Retrieves and removes an element at the front of this queue, if one exists.
+   * Throws {@link java.util.NoSuchElementException} if the queue is empty.
+   * <br><br>
+   * <b>NOTE</b> It is not exactly important where in the underlying ordered collection
+   * data structure this operation occurs at, as long as it is at the logical opposite end
+   * of the location where {@link BasicQueue#enqueue(Object)} would occur at.
    *
    * @return the head of this queue
    * @throws java.util.NoSuchElementException if this queue is empty
